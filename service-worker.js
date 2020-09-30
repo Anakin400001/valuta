7ce0bcd54fa14fe612a08db3c9dfb43e5bf98b97
Anakin400001 <58471681+Anakin400001@users.noreply.github.com>
2020-09-30 10:12:43 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.763caa85a88559ea18129642b1c831e4.js"
+  "./precache-manifest.8c93beed481a9b0eb1c8507ca1411dbe.js"
 );
 
 self.addEventListener('message', (event) => {
