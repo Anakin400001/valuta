9375baf8a2237a5362380d2889d0347c403bf8f8
Anakin400001 <58471681+Anakin400001@users.noreply.github.com>
2020-10-14 14:46:08 +0300

    Updates

---
+++ aft-preroll.js
@@ -0,0 +1,22 @@
+function user_info(user) {
+ 
+    var user_id = user.id;   // user's id
+    var app_id = 7613304;  // your app's id
+ 
+    admanInit({
+      user_id: user_id,
+      app_id: 7613304,
+      mobile: true,
+      type: 'rewarded',
+      params: {preview: 1}
+    }, onAdsReady, onNoAds);
+ 
+    function onAdsReady(adman) {
+      adman.onStarted(function () {console.log(`Начался просмотр рекламы`)});
+      adman.onCompleted(function() {console.log(`Вы досмотрели до конца`)});
+      adman.onSkipped(function() {console.log(`Вы пропустили`)});      
+      adman.onClicked(function() {}); 
+      adman.start('preroll');
+    };
+    function onNoAds() {};
+}
