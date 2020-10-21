53fbc2c010861224595f30b95f8da776ebb4043d
Anakin400001 <58471681+Anakin400001@users.noreply.github.com>
2020-10-21 12:20:04 +0300

    Updates

---
+++ precache-manifest.b075b28c438cc521028b74bc2f276d94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47f598cb1aa4451002309a125005cf8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c5ee594cbd589925cb5",
+    "url": "/static/js/2.a8fa564b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.a8fa564b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4db406822e61a854470",
+    "url": "/static/js/main.507b14c6.chunk.js"
+  },
+  {
+    "revision": "87655240af82930ed55e",
+    "url": "/static/js/runtime-main.5bff0ffa.js"
+  }
+]);
